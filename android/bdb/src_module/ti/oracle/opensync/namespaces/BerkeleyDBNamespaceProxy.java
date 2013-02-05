03c45fa1fd9e1c35cdca7af8cac97372e865bb93
Jeff English <jeff@utmostlogic.com>
2013-02-04 18:22:17 -0600

    [MOD-1249] Implemented Berkeley DB support with parity to Titanium Database API

---
+++ android/bdb/src_module/ti/oracle/opensync/namespaces/BerkeleyDBNamespaceProxy.java
@@ -0,0 +1,34 @@
+/**
+ * Oracle Opensync Module
+ * Copyright (c) 2009-2013 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ *
+ */
+
+package ti.oracle.opensync.namespaces;
+
+import org.appcelerator.kroll.KrollProxy;
+import org.appcelerator.kroll.annotations.Kroll;
+
+import SQLite.Exception;
+
+import ti.oracle.opensync.proxies.BerkeleyDBProxy;
+
+@Kroll.proxy
+public class BerkeleyDBNamespaceProxy extends KrollProxy
+{
+	// Database API constants
+	@Kroll.constant public static final int FIELD_TYPE_UNKNOWN = -1;
+	@Kroll.constant public static final int FIELD_TYPE_STRING = 0;
+	@Kroll.constant public static final int FIELD_TYPE_INT = 1;
+	@Kroll.constant public static final int FIELD_TYPE_FLOAT = 2;
+	@Kroll.constant public static final int FIELD_TYPE_DOUBLE = 3;
+
+	// Database
+	@Kroll.method
+	public BerkeleyDBProxy open(Object file) throws Exception
+	{
+		return BerkeleyDBProxy.open(file);
+	}
+}
